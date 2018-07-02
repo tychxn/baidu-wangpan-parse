dfed1ef9b14c94d7bb919fd8626c8ea76ce18d75
tychxn <tychxn@gmail.com>
2018-07-02 22:23:11 +0800

    Enable recognition if password is missed

--- baidu_wangpan_parse.py
+++ baidu_wangpan_parse.py
@@ -74,18 +74,22 @@ class BaiduWangpan(object):
         return True if js['errno'] == 0 else False
 
     def getParams(self):  # reload the page to get the parameters needed later
-        resp = self.sess.get(self.link, headers=self.headers)
-        resp.encoding = 'utf-8'
-        m = re.search('\"sign\":\"(.+?)\"', resp.text)
-        self.sign = m.group(1)
-        m = re.search('\"timestamp\":(.+?),\"', resp.text)
-        self.timestamp = m.group(1)
-        m = re.search('\"shareid\":(.+?),\"', resp.text)
-        self.primaryId = m.group(1)
-        m = re.search('\"uk\":(.+?),\"', resp.text)
-        self.uk = m.group(1)
-        m = re.search('\"fs_id\":(.+?),\"', resp.text)
-        self.fidList = '[' + m.group(1) + ']'
+        try:
+            resp = self.sess.get(self.link, headers=self.headers)
+            resp.encoding = 'utf-8'
+            m = re.search('\"sign\":\"(.+?)\"', resp.text)
+            self.sign = m.group(1)
+            m = re.search('\"timestamp\":(.+?),\"', resp.text)
+            self.timestamp = m.group(1)
+            m = re.search('\"shareid\":(.+?),\"', resp.text)
+            self.primaryId = m.group(1)
+            m = re.search('\"uk\":(.+?),\"', resp.text)
+            self.uk = m.group(1)
+            m = re.search('\"fs_id\":(.+?),\"', resp.text)
+            self.fidList = '[' + m.group(1) + ']'
+            return True
+        except Exception as e:
+            return False
 
     def getVerifyCode(self):
         print u'Start downloading the verification code...'
@@ -164,8 +168,11 @@ class BaiduWangpan(object):
             if self.isEncrypt:
                 if not self.verifyPassword():  # verify file password
                     print u'Sharing file password error!'
-                    return
-            self.getParams()
+                    return None
+
+            if not self.getParams():
+                print u'It seems that the file needs password'
+                return None
 
             # try to get the download link for the first time (without verify code)
             js = self.getRespJson(needVerify=False)
@@ -178,8 +185,8 @@ class BaiduWangpan(object):
                 else:
                     print u'Unknown error, the error code is as follows:'
                     print js
-                    return
-        except Exception as e:
+                    return None
+        except Exception:
             print 'Exception:', e
             raise
 
